2c97015898d7084d5f9515912f68712db37f1a90
helma trindade <helmacaires28@gmail.com>
2023-02-07 18:24:05 -0300

    Corrige lint

--- src/tests/helpers/walletForm.test.js
+++ src/tests/helpers/walletForm.test.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import { getByTestId, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import WalletForm from '../../components/WalletForm';
+// import WalletForm from '../../components/WalletForm';
 import { renderWithRouterAndRedux } from './renderWith';
+import Wallet from '../../pages/Wallet';
 
 test('teste se o componente Wallet renderiza um campo para adicionar valor da despesa e se ele recebe um valor', () => {
-  renderWithRouterAndRedux(<WalletForm />);
+  renderWithRouterAndRedux(<Wallet />);
 
-  const inputDespesa = screen.getByTestId('value-input');
+  const inputValor = screen.getByTestId('value-input');
+  expect(inputValor).toBeInTheDocument();
 
-  expect(inputDespesa).toBeInTheDocument();
+  userEvent.type(inputValor, '20');
 });
 
-// test('', () => {
+// test('teste se há um campo para selecionar em qual moeda será registrada a despesa', () => {
+//   renderWithRouterAndRedux(<WalletForm />);
 
+//   const inputDespesa = screen.getByTestId('currency-input');
+
+//   expect(inputDespesa).toBeInTheDocument();
 // });
 
-// test('', () => {
+// test('teste se há um campo para adicionar qual método de pagamento será utilizado', () => {
+//   renderWithRouterAndRedux(<WalletForm />);
 
+//   const moeda = screen.getByTestId('currency-input');
+
+//   expect(moeda).toBeInTheDocument();
 // });
 
-// test('', () => {
+test('verifique se é renderizado um botão na tela e se ao clicar é chamada a função handleClick ', () => {
+  renderWithRouterAndRedux(<Wallet />);
 
-// });
+  const btn = screen.getByRole('button', { name: /Adicionar despesa/i });
+  expect(btn).toBeInTheDocument();
+
+  const inputValor = screen.getByTestId('value-input');
+  userEvent.type(inputValor, '10');
+
+  const inputDescricao = screen.getByTestId('description-input');
+  userEvent.type(inputDescricao, 'comida');
+});
